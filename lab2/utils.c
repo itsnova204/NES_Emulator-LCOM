ede3e639511336d5deeb68f6bdc2223a10ad6236
Ricardo Miguel Yang <up202208465@fe.up.pt>
2024-02-23 15:38:08 +0000

    implemented utils.c

--- lab2/utils.c
+++ lab2/utils.c
@@ -2,23 +2,28 @@
 
 #include <stdint.h>
 
+//val -> valor de entrada com 16 bits (2 bytes)
+//lsb -> byte menos significativo (ultimo byte) [LEAST SIGNIFICANT BYTE]
+//OxFF -> máscara para tornar zero todos os bytes exceto o lsb
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if (lsb == NULL) return 1;
+  *lsb = (uint8_t) (val & 0xFF);
+  return 0;
 }
 
+//msb -> [MOST SIGNIFICANT BYTE]
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if (msb == NULL) return 1;
+  *msb = (uint8_t) (val >> 8);
+  return 0;
 }
 
+//Objetivo é transformar 32 bits em 8 bits
+//INT -> quantidade de bits é sempre igual à arquitetura
 int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  if (value == NULL) return 1;
+  uint32_t val;
+  int ret = sys_inb(port, &val);  
+  *value = val & 0xFF;
+  return ret;
 }
