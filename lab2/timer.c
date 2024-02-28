694798e13083b0af25d0eca26649cbcd9ce15052
Ricardo Miguel Yang <up202208465@fe.up.pt>
2024-02-28 11:10:03 +0000

    update timer.c

--- lab2/timer.c
+++ lab2/timer.c
@@ -6,9 +6,6 @@
 //sys_out -> escrever dados numa porta
 //sys_int -> ler dados de uma porta
 
-int hook_id = 0;
-int counter = 0;
-
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
   //base freq is 1193182 Hz
   //timer has 16 bits, maximum divisor is 65535 (0XFFFF)
@@ -53,76 +50,37 @@ int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
   //avisar o i8254 que vamos configurar o timer
   if (sys_outb(TIMER_CTRL, controlWord) != 0) return 1;
 
-    // Avisamos o i8254 que vamos configurar o timer
-  if (sys_outb(TIMER_CTRL, controlWord) != 0) return 1;
-
-  // Injetamos o valor inicial do contador (lsb seguido de msb) diretamente no registo correspondente
+  // Injetar o valor inicial do contador (lsb seguido de msb) diretamente no registo correspondente
   if (sys_outb(selectedTimer, LSB) != 0) return 1;
   if (sys_outb(selectedTimer, MSB) != 0) return 1;
   return 0;
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
-  if( bit_no == NULL) return 1; // o apontador deve ser válido
-  *bit_no = BIT(hook_id);       // a função que chamou esta deve saber qual é a máscara a utilizar
-  if (sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook_id) != 0) return 1; // subscrição das interrupções
-  return 0;
+  if( bit_no == NULL) return 1;   // validar o apontador
+  //todo
+  return 1;
 }
 
 int (timer_unsubscribe_int)() {
-  if (sys_irqrmpolicy(&hook_id) != 0) return 1; // desligar as interrupções
-  return 0;
+  //todo
+  return 1;
 }
 
 void (timer_int_handler)() {
-  counter++;
+  //todo
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  if (st == NULL || timer > 2 || timer < 0) return 1;
-  uint8_t RBC = (TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer));
-  if (sys_outb(TIMER_CTRL, RBC) != 0) return 1;
-  if (util_sys_inb(TIMER_0 + timer, st)) return 1;
-  return 0;
+  if (st == NULL || timer > 2 || timer < 0) return 1; //validar o timer e o st
+
+  //todo
+  return 1;
 }
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
 
-  union timer_status_field_val data;
-
-  switch (field) {
-    case tsf_all: 
-      data.byte = st; 
-      break;
-
-    case tsf_initial:                                       
-      st = (st >> 4) & 0x03;
-
-      if (st == 1) data.in_mode = LSB_only;
-      else if (st == 2) data.in_mode = MSB_only;
-      else if (st == 3) data.in_mode = MSB_after_LSB;
-      else data.in_mode = INVAL_val;
-      
-      break;
-
-    case tsf_mode:
-      st = (st >> 1) & 0x07;
-
-      if(st == 6) data.count_mode = 2;
-      else if(st == 7) data.count_mode = 3;
-      else data.count_mode = st;
-
-      break;
-      
-    case tsf_base:
-      data.bcd = st & TIMER_BCD;
-      break;        
-
-    default:
-      return 1;
-  }
-
-  if (timer_print_config(timer, field, data) != 0) return 1;
-  return 0;
+  //todo
+  return 1;
 }
