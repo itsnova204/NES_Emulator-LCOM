3f9bc68f03fc7565312e86b2e9b067cba78ba7bc
Tiago Aleixo <tiago20aleixo@gmail.com>
2024-02-28 11:16:29 +0000

    implemented utils.c and timer.c timer_get_conf()

---
+++ lab2/timer.c
@@ -0,0 +1,52 @@
+#include <lcom/lcf.h>
+#include <lcom/timer.h>
+
+#include <stdint.h>
+
+#include "i8254.h"
+
+int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+int (timer_subscribe_int)(uint8_t *bit_no) {
+    /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+int (timer_unsubscribe_int)() {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+void (timer_int_handler)() {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+}
+
+int (timer_get_conf)(uint8_t timer, uint8_t *st) {
+  if (st == NULL) return 1;
+  
+  uint8_t rb_command = TIMER_RB_CMD & TIMER_RB_COUNT_ & TIMER_RB_STATUS_ & TIMER_RB_SEL(timer);
+  
+  if(sys_outb(TIMER_CTRL,rb_command) != 0) return 1;
+
+  if(util_sys_inb(TIMER_0 + timer, st) != 0) return 1;
+
+  return 0;
+}
+
+int (timer_display_conf)(uint8_t timer, uint8_t st,
+                        enum timer_status_field field) {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
---
+++ lab2/utils.c
@@ -0,0 +1,37 @@
+#include <lcom/lcf.h>
+
+#include <stdint.h>
+
+#define 	BIT(n)   (1 << (n))
+
+int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
+  if (lsb == NULL) return 1;
+
+  uint16_t mask = 0x00FF;
+  *lsb = mask & val;
+
+  return 0;
+}
+
+int(util_get_MSB)(uint16_t val, uint8_t *msb) {
+  if (msb == NULL) return 1;
+  
+
+  *msb = val >> 8;
+
+  return 0;
+}
+
+int (util_sys_inb)(int port, uint8_t *value) {
+  if (value == NULL) return 1;
+
+  uint32_t tempVal;
+  
+  if(sys_inb(port, tempVal) == 1){
+    return 1;
+  }
+
+  *value = (uint8_t) tempVal;
+  
+  return 0;
+}
