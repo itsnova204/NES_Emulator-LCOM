c69aec0c4ba4257e582a0335f5e58309838acf6b
Tiago Aleixo <tiago20aleixo@gmail.com>
2024-03-06 11:07:48 +0000

    finished lab2

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+extern int counter;
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -40,15 +41,44 @@ int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  return timer_set_frequency(timer, freq);
 }
 
 int(timer_test_int)(uint8_t time) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  int ipc_status, r;
+  message msg;
+  uint8_t irq_set;
 
-  return 1;
+  if (timer_subscribe_int(&irq_set) != 0)
+    return 1;
+
+
+
+  while( time>0 ) { /* You may want to use a different condition */
+      /* Get a request message. */
+      if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) { 
+          printf("driver_receive failed with: %d", r);
+          continue;
+      }
+      if (is_ipc_notify(ipc_status)) { /* received notification */
+          switch (_ENDPOINT_P(msg.m_source)) {
+              case HARDWARE: /* hardware interrupt notification */				
+                  if (msg.m_notify.interrupts & irq_set) { /* subscribed interrupt */
+                      timer_int_handler();//increases counter of how many interrupts were received;
+                      if(counter%60==0){
+                        timer_print_elapsed_time();
+                        time--;
+                      }
+                  }
+                  break;
+              default:
+                  break; /* no other notifications expected: do nothing */	
+          }
+      } else { /* received a standard message, not a notification */
+          /* no standard messages expected: do nothing */
+      }
+  }
+
+  
+  return timer_unsubscribe_int();
 }
