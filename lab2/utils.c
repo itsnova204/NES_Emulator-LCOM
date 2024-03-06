df2d5c1c677f1952323ce3486712de2a48888bf2
Ricardo Yang <ricardo.yang00@gmail.com>
2024-03-06 11:09:04 +0000

    test sys_inb

--- lab2/utils.c
+++ lab2/utils.c
@@ -23,7 +23,7 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
 int (util_sys_inb)(int port, uint8_t *value) {
   if (value == NULL) return 1;
   uint32_t temp;
-  sys_inb(port, &temp);  
+  if (sys_inb(port, &temp) != 0) return 1;  
   *value = temp & 0xFF;
   return 0;
 }
