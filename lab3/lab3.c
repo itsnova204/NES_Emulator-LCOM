0dd05c9bbaec8c06c5c0765d74916f0196700d58
Ricardo Yang <ricardo.yang00@gmail.com>
2024-03-19 21:26:00 +0000

    lab3: poll passed

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -74,11 +74,16 @@ int(kbd_test_scan)() {
 }
 
 int(kbd_test_poll)() {
-  uint8_t scan_code = 0 ;
+  uint8_t scan_code = 0, prev_scan_code = 0;
+  
   while (scan_code != KBD_ESC_BREAK_CODE) {
-    if (!kbc_read_output(KBD_OUT_BUF, &scan_code)) {
+    if (kbc_read_output(KBD_OUT_BUF, &scan_code)) break;
+    
+    if (scan_code != prev_scan_code) {
       kbd_print_scancode(!(KBD_MAKE_CODE & scan_code), getScanCodeSize(scan_code), &scan_code);
+      prev_scan_code = scan_code;
     }
+    //kbd_print_scancode(!(KBD_MAKE_CODE & scan_code), getScanCodeSize(scan_code), &scan_code);
   }
   return kbd_restore();
 }
