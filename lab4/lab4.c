0bdff2a15f3da5fd35b18d94d04590ce4575e3f1
Tiago Aleixo <tiago20aleixo@gmail.com>
2024-03-22 19:21:19 +0000

    implemented mouse_update_packet and mouse_store_bytes

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#include "mouse.h"
+
 // Any header files included below this line should have been created by you
 
 int main(int argc, char *argv[]) {
@@ -32,11 +34,15 @@ int main(int argc, char *argv[]) {
 
 
 int (mouse_test_packet)(uint32_t cnt) {
-  uint8_t mouse_irq_set;
+    uint8_t mouse_irq_set;
+
+    int ipc_status, r;
+    message msg;
 
     if (mouse_subscribe_int(&mouse_irq_set) != 0) return 1;
     if (mouse_enable_data_reporting() != 0) return 1;
 
+    struct packet mouse_packet;   
 
     while(cnt > 0) { /* You may want to use a different condition */
       /* Get a request message. */
@@ -48,11 +54,13 @@ int (mouse_test_packet)(uint32_t cnt) {
           switch (_ENDPOINT_P(msg.m_source)) {
               case HARDWARE: /* hardware interrupt notification */				
                   if (msg.m_notify.interrupts & mouse_irq_set) { /* subscribed interrupt */
-                      kbc_ih();
                       mouse_ih();
-                      //read 3 bytes
-                        //print
-                        //cnt--;
+                      if (mouse_store_bytes() == 3){
+                        mouse_packet = get_mouse_packet();
+                        mouse_print_packet(&mouse_packet);
+                        reset_mouse_packet_counter();
+                        cnt--;
+                      }
                   }
                   break;
               default:
@@ -63,6 +71,7 @@ int (mouse_test_packet)(uint32_t cnt) {
       }
   }
 
+    mouse_unsubscribe_int();
     return 0;
 }
 
@@ -72,7 +81,7 @@ int (mouse_test_async)(uint8_t idle_time) {
     return 1;
 }
 
-int (mouse_test_gesture)() {
+int (mouse_test_gesture)(uint8_t x_len, uint8_t tolerance) {
     /* To be completed */
     printf("%s: under construction\n", __func__);
     return 1;
---
+++ lab4/timer_utils.h
@@ -0,0 +1,7 @@
+#ifndef __TIMER_UTILS_H_
+#define __TIMER_UTILS_H_
+ 
+int get_counter();
+void set_counter(int value);
+
+#endif // __TIMER_UTILS_H_
