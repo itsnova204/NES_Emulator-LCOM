9cf893f29be37dbf0f4618e2720280c91982e2a0
Tiago Aleixo <tiago20aleixo@gmail.com>
2024-04-05 18:41:56 +0100

    added cartridge to system bus

--- proj/src/bus.c
+++ proj/src/bus.c
@@ -22,31 +22,28 @@ void bus_clock(){
 	mainClockCounter++;
 }
 
-void mainBus_write(uint16_t addr, uint8_t data){
-  if (addr <= 0x1FFF){ //access sys_ram
-    sys_ram[addr & 0x07FF] = data;
-  }else if (addr >= 0x2000 && addr <= 0x3FFF){ //access ppu registers
-    ppuBus_write(addr, data);
+uint8_t sysBus_read(uint16_t addr){
+  if (addr >= 0x8000){ //access cartridge
+    return sys_readFromCard(addr);
   }
-}
-
-uint8_t mainBus_read(uint16_t addr){
   if (addr <= 0x1FFF){ //access sys_ram
     return sys_ram[addr & 0x07FF];
-
-  }else if (addr >= 0x2000 && addr <= 0x3FFF){ //access ppu registers
-    return ppuBus_read(addr & 0x0007);
-
+  }
+  if (addr >= 0x2000 && addr <= 0x3FFF){ //access ppu registers
+    return sys_readFromPPU(addr & 0x0007);
   }
 
   return 0;
 }
 
-void ppuBus_write(uint16_t addr, uint8_t data){
+void sysBus_write(uint16_t addr, uint8_t data){
+  if (addr >= 0x8000){ //access cartridge
+    return sys_writeToCard(addr, data);
 
-}
+  }else if (addr <= 0x1FFF){ //access sys_ram
+    sys_ram[addr & 0x07FF] = data;
 
-uint8_t ppuBus_read(uint16_t addr){
-
-  return 0;
+  }else if (addr >= 0x2000 && addr <= 0x3FFF){ //access ppu registers
+    sys_writeToPPU(addr, data);
+  }
 }
