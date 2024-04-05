b179da0c5b207e794225e6f8d1c9b8e3df9059d1
Tiago Aleixo <tiago20aleixo@gmail.com>
2024-04-05 17:31:54 +0100

    created files for dev of the picture processing unit or PPU

---
+++ proj/src/ppu.c
@@ -0,0 +1,18 @@
+/*=================================================================
+=========================== rX2C0XX ppu ===========================
+===================================================================
+==Emulator for the PPU (picture processing unit) used in the NES ==
+==DOCS: https://www.nesdev.org/wiki/PPU                          ==
+==                                                               ==
+== Tiago Aleixo, 2024                                            ==
+=================================================================*/
+#include <lcom/lcf.h>
+#include "ppu.h"
+#include "bus.h"
+
+uint8_t ppu_nametable_ram[2][1024]; //2kb of ram for nametables
+uint8_t ppu_palette_ram[32]; //32 bytes of ram for palette
+
+void ppu_clock(){
+  
+}
