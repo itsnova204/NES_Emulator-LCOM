12607b425553ca42b856c0b2d28b3093f04eec72
Tiago Aleixo <tiago20aleixo@gmail.com>
2024-04-05 18:22:02 +0100

    finished mapper000

--- proj/src/mapper.c
+++ proj/src/mapper.c
@@ -1,7 +1,7 @@
 #include "mapper.h"
 
 static uint8_t mapper_id;
-static uint16_t (*mapper_table[])(uint16_t) = {&mapper_000};
+static uint16_t (*mapper_table[])(uint16_t, enum Type) = {&mapper_000};
 
 static uint8_t mapper_nPRGbanks;
 static uint8_t mapper_nCHRbanks;
@@ -14,13 +14,26 @@ uint8_t set_mapper(uint8_t id, uint8_t nPRGbanks, uint8_t nCHRbanks){
   return 0;
 }
 
-uint16_t mapper_map(uint16_t addr){
-  return mapper_table[mapper_id](addr);
+uint16_t mapper_map(uint16_t addr, enum Type type){
+  return mapper_table[mapper_id](addr, type);
 }
 
-static uint16_t mapper_000(uint16_t addr){//TODO finish implementing this
-  if (addr >= 0x8000 && addr <= 0xFFFF){
+uint16_t mapper_000(uint16_t addr, enum Type type){ //bounds are checked in the cartridge
+  switch (type){
+  case sys_read:
+      return addr & (mapper_nPRGbanks > 1 ? 0x7FFF : 0x3FFF);
+    break;
+  case sys_write:
     return addr & (mapper_nPRGbanks > 1 ? 0x7FFF : 0x3FFF);
+    break;
+  case ppu_read:
+      return addr;
+      break;
+  case ppu_write:
+      return addr; 
+      break; 
+  default:
+    break;
   }
-  return addr;
+  return 0;
 }
--- proj/src/mapper.h
+++ proj/src/mapper.h
@@ -1,7 +1,14 @@
 #include "lcf_mock.h"
 #pragma once
 
-uint8_t set_mapper(uint8_t id, uint8_t nPRGbanks, uint8_t nCHRbanks);
-uint16_t mapper_map(uint16_t addr);
+enum Type{
+  sys_read,
+  sys_write,
+  ppu_read,
+  ppu_write,
+};
 
-static uint16_t mapper_000(uint16_t addr);
+uint8_t set_mapper(uint8_t id, uint8_t nPRGbanks, uint8_t nCHRbanks);
+uint16_t mapper_map(uint16_t addr, enum Type type);
+
+uint16_t mapper_000(uint16_t addr, enum Type type);
