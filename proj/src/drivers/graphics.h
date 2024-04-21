7ec477366998c94ab83d91c28deba9f440a3da68
Tiago Aleixo <tiago20aleixo@gmail.com>
2024-04-21 17:25:10 +0100

    added driver files from labs

---
+++ proj/src/drivers/graphics.h
@@ -0,0 +1,7 @@
+#ifndef __graphics__
+#define __graphics__
+
+int vg_init_mode(uint16_t mode);
+int vg_init_framebuffer(uint16_t mode);
+
+#endif
---
+++ proj/src/drivers/timer_utils.h
@@ -0,0 +1,7 @@
+#ifndef __TIMER_UTILS_H_
+#define __TIMER_UTILS_H_
+ 
+int timer_get_counter();
+void timer_reset_counter();
+
+#endif // __TIMER_UTILS_H_
