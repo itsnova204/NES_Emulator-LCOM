193f56e009f040f0a8ff7243318aa5afa6f9e2a8
Tiago Aleixo <tiago20aleixo@gmail.com>
2024-05-14 15:53:59 +0100

    dont remember what i did, probably good

--- proj/src/nes_emu/bus.h
+++ proj/src/nes_emu/bus.h
@@ -2,7 +2,9 @@
 
 #pragma once
 
-void bus_init();
+void bus_clock();
+void bus_init(char* cart_filePath);
+int bus_exit();
 
 void sysBus_write(uint16_t addr, uint8_t data);
 uint8_t sysBus_read(uint16_t addr);
