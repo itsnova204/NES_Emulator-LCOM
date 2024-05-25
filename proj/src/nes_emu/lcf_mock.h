0d7cdba24eb74d096fcfc1fb2258250e694da990
Tiago Aleixo <tiago20aleixo@gmail.com>
2024-05-25 22:20:26 +0100

    fixed mapper not being loaded

--- proj/src/nes_emu/lcf_mock.h
+++ proj/src/nes_emu/lcf_mock.h
@@ -1,11 +1,1 @@
-#pragma once
-
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <unistd.h>
-
-#define BIT(n) (0x01 << n)
-
+#include <lcom/lcf.h>
