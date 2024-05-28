1067b3611a3bff7abab3017ee52b015213c2a8c4
Leonor Filipe Fragoso e Santos <up202204354@up.pt>
2024-05-28 23:10:46 +0100

    cleaned utils

--- proj/src/menu/utils.c
+++ proj/src/menu/utils.c
@@ -2,28 +2,34 @@
 
 #include <stdint.h>
 
-//val -> valor de entrada com 16 bits (2 bytes)
-//lsb -> byte menos significativo (ultimo byte) [LEAST SIGNIFICANT BYTE]
-//OxFF -> máscara para tornar zero todos os bytes exceto o lsb
-int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  if (lsb == NULL) return 1;
-  *lsb = (uint8_t) (val & 0xFF);
+int(util_get_LSB)(uint16_t val, uint8_t *lsb)
+{
+  if (lsb == NULL)
+    return 1;
+  *lsb = (uint8_t)val;
+
   return 0;
 }
 
-//msb -> [MOST SIGNIFICANT BYTE]
-int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  if (msb == NULL) return 1;
-  *msb = (uint8_t) (val >> 8);
+int(util_get_MSB)(uint16_t val, uint8_t *msb)
+{
+  if (msb == NULL)
+    return 1;
+  *msb = (uint8_t)(val >> 8);
+
   return 0;
 }
 
-//Leitura do valor que está no 'port' e armazenar no 'value'
-//INT -> quantidade de bits é sempre igual à arquitetura
-int (util_sys_inb)(int port, uint8_t *value) {
-  if (value == NULL) return 1;
+int(util_sys_inb)(int port, uint8_t *value)
+{ // leitura; transforma o valor de 32 bits para 8 bits
+  if (value == NULL)
+    return 1;
+
   uint32_t temp;
-  if (sys_inb(port, &temp) != 0) return 1;  
-  *value = temp & 0xFF;
+  if (sys_inb(port, &temp) != 0)
+    return 1;
+
+  *value = (uint8_t)temp;
+
   return 0;
 }
