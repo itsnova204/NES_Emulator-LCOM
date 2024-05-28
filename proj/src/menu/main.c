43344d23e1854a20e14559bd3c421207ace03451
Leonor Filipe Fragoso e Santos <up202204354@up.pt>
2024-05-29 00:05:49 +0100

    trying to incorporate mouse on menu

--- proj/src/menu/main.c
+++ proj/src/menu/main.c
@@ -8,6 +8,7 @@
 #include "i8254.h"
 #include "keyboard.h"
 #include "rtc.h"
+#include "mouse.h"
 
 #include "sprite.h"
 
@@ -17,6 +18,15 @@ uint8_t scan_code = 0;
 // DEFINE FPS
 #define FPS 60
 #define FRAME_INTERVAL (60 / FPS)
+
+int get_selected_option(int mouse_x, int mouse_y) {
+    if (mouse_x >= 100 && mouse_x <= 300) {
+        if (mouse_y >= 100 && mouse_y < 200) return 1;
+        if (mouse_y >= 200 && mouse_y < 300) return 2;
+        if (mouse_y >= 300 && mouse_y < 400) return 3;
+    }
+    return 0;
+}
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -52,18 +62,20 @@ int (proj_main_loop)() {
   // SETUP e ciclo de interrupcoes do timer e do teclado
   int ipc_status, r;
   message msg;
-  uint8_t irq_set_timer, irq_set_kbd;
+  uint8_t irq_set_timer, irq_set_kbd, irq_set_mouse;
 
   if(timer_subscribe_int(&irq_set_timer) != 0) return 1;
   if(kbd_subscribe_int(&irq_set_kbd) != 0) return 1;
+  if(mouse_subscribe_int(&irq_set_mouse) != 0) return 1;
 
-  if (timer_set_frequency(0,60) != 0) return 1;   
+  if (timer_set_frequency(0, 60) != 0) return 1;   
 
   bool is_second_scan_code = false;
   rtc_read_date();
-  while( scan_code != KBD_ESC_BREAK_CODE ) {
+
+  while(scan_code != KBD_ESC_BREAK_CODE) {
       /* Get a request message. */
-      if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) { 
+      if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) { 
           printf("driver_receive failed with: %d", r);
           continue;
       }
@@ -74,7 +86,8 @@ int (proj_main_loop)() {
                   if (msg.m_notify.interrupts & irq_set_kbd) {
                     kbc_ih();
                     if (!is_valid()) continue;
-                    scan_code = get_scan_code();
+                    scan_code = get_scancode();
+
                     if (is_two_byte_scan_code(scan_code) && !is_second_scan_code) {
                       is_second_scan_code = true;
                       continue;
@@ -106,7 +119,22 @@ int (proj_main_loop)() {
 
                       swap_buffers();
                     }
+                  }
 
+                  if (msg.m_notify.interrupts & irq_set_mouse) {
+                    mouse_int_handler();
+
+                    if (mouse_sync()) {
+                      struct packet pp = get_mouse_packet();
+
+                        if (pp.lb) {
+                        int option = get_selected_option(pp.delta_x, pp.delta_y);
+                        if (option) {
+                          printf("Selected option: %d\n", option);
+                          // jogo
+                        }
+                      }
+                    }
                   }
                   break;
               default:
@@ -118,6 +146,7 @@ int (proj_main_loop)() {
   if (vg_exit() != 0) return 1;
   if (timer_unsubscribe_int() != 0) return 1;
   if (kbd_unsubscribe_int() != 0) return 1;
+  if (mouse_unsubscribe_int() != 0) return 1;
 
   return 0;
 }
