7e016df960e9a3146e8d0fc91464488e2441fd2f
Ricardo Yang <ricardo.yang00@gmail.com>
2024-06-01 16:49:47 +0100

    sprite: refactoring and documentation

--- proj/src/menu/sprite.h
+++ proj/src/menu/sprite.h
@@ -44,16 +44,26 @@ void (preloadSprites)(uint16_t vg_mode);
 
 /**
  * @brief Returns the xpm data of a given image name
+ * @param name Name of the image
+ * @return Pointer to the xpm data of the image, NULL if the image is not found
 */
 XpmData* (get_xpm_data)(ImageName name);
 
 /**
  * @brief Draws a sprite on the screen
+ * @param name Name of the image to draw
+ * @param x X coordinate of the image
+ * @param y Y coordinate of the image
+ * @return 0 if successful, non 0 otherwise
 */
 int (draw_sprite)(ImageName name, uint16_t x, uint16_t y);
 
 /**
  * @brief Draws a sprite on the screen from the bottom left corner
+ * @param name Name of the image to draw
+ * @param x X coordinate of the image
+ * @param y Y coordinate of the image
+ * @return 0 if successful, non 0 otherwise
 */
 int (draw_sprint_from_bottom_left)(ImageName name, uint16_t x, uint16_t y);
 
@@ -67,6 +77,7 @@ int (draw_sprint_from_bottom_left)(ImageName name, uint16_t x, uint16_t y);
  * @param x X coordinate of the date
  * @param y Y coordinate of the date
  * @param with_colon If true, the colon between the hours and minutes will be drawn. This parameter is to be able to have the blinking effect of the colon
+ * @return 0 if successful, non 0 otherwise
 */
 int (draw_date)(int day, int month, int year, int hour, int minutes, uint16_t x, uint16_t y, bool with_colon);
 
@@ -76,6 +87,7 @@ int (draw_date)(int day, int month, int year, int hour, int minutes, uint16_t x,
  * @param x_mouse X coordinate of the mouse
  * @param y_mouse Y coordinate of the mouse
  * @param selected_option Pointer to the variable that will store the selected option, value will be -1 if no option is selected
+ * @return 0 if successful, non 0 otherwise
 */
 int (draw_options)(uint16_t y, uint16_t x_mouse, uint16_t y_mouse, int* selected_option);
 
